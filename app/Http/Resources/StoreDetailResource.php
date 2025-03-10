46221aae3d3b5a169bff748adcf0d476bc3c7b6e
ADMIN-PC\Admin <huesoftmobile>
2025-03-10 20:18:34 +0700

    app config

--- app/Http/Resources/StoreDetailResource.php
+++ app/Http/Resources/StoreDetailResource.php
@@ -39,6 +39,9 @@ class StoreDetailResource extends JsonResource
             "is_open" => Store::isStoreOpen($this->id),
             "active" => $this->active,
             "operating_hours" => $this->operating_hours,
+            "card_bank" => $this->operating_hours,
+            "card_number" => $this->operating_hours,
+            "card_holder_name" => $this->operating_hours,
 
             "address" => $this->address,
             "street" => $this->street,

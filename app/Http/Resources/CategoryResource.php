20f4e51a82ed37999d3e860bc31b89f9cc444f0f
ADMIN-PC\Admin <huesoftmobile>
2025-03-13 16:01:33 +0700

    update

--- app/Http/Resources/CategoryResource.php
+++ app/Http/Resources/CategoryResource.php
@@ -19,7 +19,7 @@ class CategoryResource extends JsonResource
             'id' => $this->id,
             'name' => LocalizationHelper::getNameByLocale($this),
             'image' => $this->image,
-            'children' => ProductResource::collection($this->products)
+            'products' => ProductResource::collection($this->products)
         ];
     }
 }

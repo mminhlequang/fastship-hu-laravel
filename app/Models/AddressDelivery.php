171b2dd51453744ff6638c99d86be6e3bc2328ca
ADMIN-PC\Admin <huesoftmobile>
2025-04-01 18:28:57 +0700

    update

--- app/Models/AddressDelivery.php
+++ app/Models/AddressDelivery.php
@@ -18,14 +18,16 @@ class AddressDelivery extends Model
     ];
     // Cast attributes JSON to array
     protected $casts = [
-        'is_default' => 'integer'
+        'is_default' => 'integer',
+        'lat' => 'float',
+        'lng' => 'float'
     ];
 
     protected $primaryKey = 'id';
 
     protected $fillable = ['name', 'phone', 'address', 'is_default', 'customer_id',
         'phone', 'street', 'zip', 'city', 'state', 'country', 'country_code', 'lat', 'lng', 'deleted_at'
-        ];
+    ];
 
 
     public function customer()

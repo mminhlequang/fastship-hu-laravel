6223d66c8c67ec43e7108250e2637fb425279e78
ADMIN-PC\Admin <huesoftmobile>
2025-04-03 20:32:45 +0700

    update

--- routes/api.php
+++ routes/api.php
@@ -12,6 +12,11 @@ use Illuminate\Http\Request;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
+header('Access-Control-Allow-Origin:  *');
+header('Access-Control-Allow-Methods:  POST, GET, OPTIONS, PUT, DELETE');
+header('Access-Control-Allow-Headers:  Content-Type, X-Auth-Token, Origin, Authorization');
+
+
 Route::prefix('v1')->middleware(['language'])->group(function () {
     //** API-Auth */
     Route::post('/login', 'Api\CustomerController@login');

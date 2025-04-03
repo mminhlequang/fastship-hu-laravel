b46966ae64b7601a0a8881f986f8d86ef0e863b8
ADMIN-PC\Admin <huesoftmobile>
2025-04-03 20:16:30 +0700

    update

--- app/Http/Middleware/VerifyCsrfToken.php
+++ app/Http/Middleware/VerifyCsrfToken.php
@@ -13,8 +13,6 @@ class VerifyCsrfToken extends Middleware
      */
     protected $except = [
         'ckfinder/*',
-        'ajaxPost/*',
-        'ajaxPost/customerRegister',
-        'customer-register/*'
+        'api/v1/*'
     ];
 }

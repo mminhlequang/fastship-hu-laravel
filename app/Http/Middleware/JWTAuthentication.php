9cc22fd910e3aba4113ba507cf874a345538b41e
ADMIN-PC\Admin <huesoftmobile>
2025-04-03 19:27:09 +0700

    update

--- app/Http/Middleware/JWTAuthentication.php
+++ app/Http/Middleware/JWTAuthentication.php
@@ -18,16 +18,8 @@ class JWTAuthentication
     public function handle(Request $request, Closure $next)
     {
         try {
-            $payload = JWTAuth::parseToken()->getPayload();
-            $uid = $payload->get('uid');
-
-            $customer = \App\Models\Customer::where('uid', $uid)->first();
-
-            if (!$customer) {
+            if (!auth('api')->check())
                 return response()->json(['status' => false, 'message' => __('INVALID_SIGNATURE')], 404);
-            }
-
-//            auth('api')->login($customer);
 
             return $next($request);
 

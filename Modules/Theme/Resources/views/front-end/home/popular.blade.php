f371f61e34fac5a6735f70532e4e664a1aa7ec6e
ADMIN-PC\Admin <huesoftmobile>
2025-04-04 20:18:49 +0700

    update

--- Modules/Theme/Resources/views/front-end/home/popular.blade.php
+++ Modules/Theme/Resources/views/front-end/home/popular.blade.php
@@ -11,7 +11,7 @@
                     <div class="relative swiper-slide rounded-2xl mt-4">
                         <div class="skeleton absolute inset-0 bg-gray-200 z-50"></div>
                         <div class="rounded-2xl bg-white p-4 flex flex-col gap-8 w-full cursor-pointer transition-all hover:shadow-[0_2px_0_0_#75ca45,0_-2px_0_0_#75ca45,-2px_0_0_0_#75ca45,2px_0_0_0_#75ca45,0_5px_0_0_#75ca45]">
-                            <img data-src="{{ url($itemC->name_en) }}" class="w-full lazyload" alt="Food Category" onerror="this.onerror=null; this.src='{{ url('images/no-image.png') }}'"/>
+                            <img data-src="{{ url($itemC->image) }}" class="w-full lazyload" alt="Food Category" onerror="this.onerror=null; this.src='{{ url('images/no-image.png') }}'"/>
 
                             <div class="flex flex-col gap-1 items-center justify-center">
                                 <h3 class="font-medium text-lg">{{ $itemC->name_en }}</h3>

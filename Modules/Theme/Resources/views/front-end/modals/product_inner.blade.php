8c0877fc2008458938026b0e2f83cd5e55ee5da5
ADMIN-PC\Admin <huesoftmobile>
2025-04-05 13:16:38 +0700

    update

--- Modules/Theme/Resources/views/front-end/modals/product_inner.blade.php
+++ Modules/Theme/Resources/views/front-end/modals/product_inner.blade.php
@@ -1,7 +1,7 @@
 @if(isset($product))
    <!-- Close Button -->
    <button
-           id="closeModalBtnProduct"
+           onclick="toggleModal('modalOverlayLogin')"
            class="absolute top-4 right-4 text-gray-500 hover:text-gray-700"
    >
       <svg

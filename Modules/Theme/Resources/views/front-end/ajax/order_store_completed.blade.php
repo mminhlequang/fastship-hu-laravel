acd7f3e864c01d376a73e4204101294a9e96c98d
ADMIN-PC\Admin <huesoftmobile>
2025-05-16 01:42:42 +0700

    update

--- Modules/Theme/Resources/views/front-end/ajax/order_store_completed.blade.php
+++ Modules/Theme/Resources/views/front-end/ajax/order_store_completed.blade.php
@@ -18,7 +18,7 @@
         <h2 class="text-orange-500 font-bold text-xl">Your Order is Ready!</h2>
         <p class="text-gray-600">The store has finished preparing your food.</p>
         <p class="text-gray-600 mb-4">You can come pick it up anytime.</p>
-        <button id="doneBtn" data-id="1" class="w-full max-w-sm py-2 bg-[#74CA45] hover:bg-primary-700 text-white font-medium rounded-full focus:outline-none">
+        <button id="doneBtn" data-id="{{ $order->id }}" class="w-full max-w-sm py-2 bg-[#74CA45] hover:bg-primary-700 text-white font-medium rounded-full focus:outline-none">
             Got it
         </button>
     </div>
